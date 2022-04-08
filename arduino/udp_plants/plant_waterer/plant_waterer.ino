34bb390dfb7f8ce6b3809e2e71ad51bccc8d1475
Donald Undeen <donaldundeen@Donalds-MacBook-Pro.local>
2022-04-08 13:42:49 -0400

    new changes

---
+++ arduino/udp_plants/plant_waterer/plant_waterer.ino
@@ -0,0 +1,282 @@
+/*
+ * Board types
+ * ESP32 Arduino -> Adafruit ESP32 Feather
+ * ESP32 Adruino -> ESP32 Dev Module
+ */
+
+/*#include <Arduino.h>
+#include <WiFi.h>
+*/
+#include <WiFi.h>
+#include <PubSubClient.h>
+// #include <ArduinoOSC.h> // you can use this if your borad supports only WiFi or Ethernet
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+// WiFi stuff
+//const char* ssid = "JJandJsKewlPad";
+//const char* pwd = "WeL0veLettuce";
+//const char* host = "10.0.0.162"; // dev laptop
+
+const char* ssid = "log.local";
+const char* pwd = "";
+//const char* host = "10.0.0.74"; // rpi when on log.local
+const char* host = "10.0.0.203"; // what you're sending messages TO
+
+//const IPAddress ip(10, 0, 0, 225);
+IPAddress ip;  // THIS device's IP  (need to tie to consistent mac addresses, with a table)
+const IPAddress gateway(10, 0, 0, 1);
+const IPAddress subnet(255, 255, 255, 0);
+
+int port = 1883;
+
+String arduinomacs[]= { 
+"40:F5:20:44:B1:3C",
+"40:F5:20:45:D5:14",
+"40:F5:20:45:D0:18",
+"C4:DD:57:9C:DC:A4",
+"40:F5:20:45:D0:A4",
+"C4:DD:57:9C:CA:20",
+"3C:61:05:4A:5F:C0",
+"rpi:mac:address",
+"ap:mac:address",
+};
+
+int arduinoips[] = {
+  224,
+  225,
+  226,
+  227,
+  228, // cyberpoop
+  229,
+  230,
+  74,
+  203,
+};
+
+int numplants = 9;
+
+String humannames[] = { 
+  "water",
+  "pinecone",
+  "swingingtree",
+  "sprout",
+  "cyberpoop",
+  "barkcycle",
+  "root",
+  "mothertree",
+  "accesspoint"
+};
+
+String ipprefix  = "10.0.0.";
+String thisarduinomac = "";
+String thishumanname = "";
+int thisarduinoip = 0;
+int sendcount = 0;
+
+void setup() {
+
+    Serial.begin(115200);
+
+    float batteryLevel = (analogRead(A13) / 4095.0) * (2.0 * 3.3 * 1.1);
+    Serial.println("-+-+-+-+-+-+- battery level is " + String (batteryLevel));
+
+    Serial.println(LED_BUILTIN);
+    Serial.println(A8);
+    pinMode(LED_BUILTIN, OUTPUT);
+      
+    delay(2000);
+
+    fastblink(3);
+    
+    thisarduinomac = WiFi.macAddress();
+    Serial.print("this mac address is ");
+    Serial.println(thisarduinomac);
+    resolveids();
+
+
+    // WiFi stuff (no timeout setting for WiFi)
+    Serial.print("connecting to SSID ");
+    Serial.println(ssid);
+   
+    connect_wifi();
+
+    setup_mqtt();
+
+    setup_sensor();
+
+  
+}
+
+
+void setup_mqtt(){
+  client.setServer(host, port);
+  client.setCallback(callback);
+}
+
+void connect_wifi(){
+   if(WiFi.status() != WL_CONNECTED){
+      Serial.println("connecting to wifi");
+#ifdef ESP_PLATFORM
+      WiFi.disconnect(true, true);  // disable wifi, erase ap info
+      delay(1000);
+      WiFi.mode(WIFI_STA);
+#endif
+  
+      WiFi.begin(ssid, pwd);
+      WiFi.config(ip, gateway, subnet);
+      
+      while (WiFi.status() != WL_CONNECTED) {
+          Serial.print(".");
+          fastblink(2);
+  //        delay(500);
+      }
+      
+      Serial.print("WiFi connected, IP = ");
+      Serial.println(WiFi.localIP());
+   }
+  
+}
+
+// beware of returning messages that were just sent.
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+  sensor_sub_callback(String(topic), messageTemp);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    client.setKeepAlive(60);    
+    char humanname_c[thishumanname.length() + 1];
+    thishumanname.toCharArray(humanname_c, thishumanname.length() + 1); 
+    if (client.connect(humanname_c)) {
+      Serial.println("connected");
+      // Subscribe
+      setup_subs();
+     } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      fastblink(4);    
+      delay(4000);
+    }
+  }
+}
+
+int count = 0;
+void loop() {
+    connect_wifi();
+    if (!client.connected()) {
+      reconnect();
+    }    
+    client.loop();
+
+    loop_sensor();
+}
+
+
+void sendMessage(char* topic, int part1){
+    connect_wifi();  
+    Serial.println("sending " + String(topic) + ":"+String(part1) );
+    char buf[5];
+    sprintf(buf, "%04i", part1);
+    client.publish(topic, buf);
+}
+
+
+void fastblink(int times){
+  for(int i = 0; i< times; i++){
+    
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(250);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(250);  
+  }
+}
+
+
+void resolveids(){
+  int foundindex = -1;
+  Serial.print("arudinomacs size is ");
+  Serial.println(sizeof(arduinomacs));
+  for (int index = 0; index < numplants; index++) {
+    if(thisarduinomac == arduinomacs[index]){
+      foundindex = index;
+      break;
+    }
+  }
+  if(foundindex >= 0){
+    thisarduinoip = arduinoips[foundindex];
+    thishumanname = humannames[foundindex];
+  }
+  ip = IPAddress(10, 0, 0, thisarduinoip);
+  Serial.print("arduinomac : " );
+  Serial.println(thisarduinomac);
+  Serial.print("ip : ");
+  Serial.println(thisarduinoip);
+  Serial.print("human name : " );
+  Serial.println(thishumanname);
+}
+
+int fsrAnalogPin = A4;
+int fsrReading  = 3;      // the analog reading from the FSR resistor divider
+
+void pre_setup_sensor(){
+  // this runs BEFORE the regular setup.
+
+}
+
+void setup_subs(){
+
+}
+
+void sensor_sub_callback(String topic, String message){
+}
+
+void setup_sensor(){
+/* A4 / 36 ( 8 up from bottom on long side) - 
+ *  this is an analog input A4 and also GPI #36. 
+ *  Note it is _not_ an output-capable pin! It uses ADC #1
+
+// 3V is 2nd down from top on long 
+// gnd is 4 down on long side
+*/
+/*
+ * Connect vcc out from HL-01 to 3V, 
+ * Connect AO out  to Analog 4 .
+ * Connect Gnd to Ground
+
+
+ */
+}
+
+/*
+Value out of soil: 4095
+Value in moist soil: ~2000, each reading +/-50 (range of 100)
+drier soil: ~3000
+ */
+void loop_sensor(){
+  Serial.println(fsrAnalogPin);
+  fsrReading = analogRead(fsrAnalogPin);
+  Serial.print("Analog reading = ");
+  Serial.println(fsrReading);
+  sendMessage("/water", fsrReading);
+  delay(5000);
+}
